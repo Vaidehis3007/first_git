2ae4b0fc9ab6b0941e77639579a668ec26af2294
“Vaidehie” <sahuvaidehi30@gmail.com>
2023-10-30 22:39:05 +0530

    Add the number swap code

---
+++ practicals/numberswap.php
@@ -0,0 +1,14 @@
+<?php
+$num1=5;
+$num2=7;
+
+echo"\nBefor swapping: ";
+echo"\nNumber 1: ".$num1.", Number 2: ".$num2;
+
+$temp=$num1;
+$num1=$num2;
+$num2=$temp;
+
+echo"\nAfter swapping: ";
+echo"\nNumber 1: ".$num1.", Number 2: ".$num2;
+?>
