7d32e4a57c6cbfbd3528d4c1c583fcc13aa5f2c0
“Vaidehie” <sahuvaidehi30@gmail.com>
2023-11-01 19:44:54 +0530

    Update hello world program for readability and maintainability

--- practicals/hello.php
+++ practicals/hello.php
@@ -1,3 +1,17 @@
 <?php
-echo"hello";
+/**
+ * Simple "Hello, World!" script in PHP
+ */
+
+// Define a constant for the message
+define('GREETING', 'Hello, World!');
+
+// Function to display the greeting
+function displayGreeting($message) {
+    echo $message;
+}
+
+// Call the displayGreeting function
+displayGreeting(GREETING);
 ?>
+>
