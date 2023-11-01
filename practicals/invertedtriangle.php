23b19ed96964b33ecd4e80ffcd26e686b9c25285
“Vaidehie” <sahuvaidehi30@gmail.com>
2023-11-01 19:57:06 +0530

    Update the inverted triangle program for readability and maintainability

--- practicals/invertedtriangle.php
+++ practicals/invertedtriangle.php
@@ -1,13 +1,20 @@
 <?php
+/**
+ * A readable and maintainable PHP code to print the pattern:
+ * *****
+ * ****
+ * ***
+ * **
+ * *
+ */
 
-$rows = 5; 
+$rows = 5; // Number of rows in the pattern
 
 for ($i = $rows; $i >= 1; $i--) {
     for ($j = 1; $j <= $i; $j++) {
-        echo "*";
+        echo '*';
     }
-    echo PHP_EOL;
+    echo "\n"; // Move to the next line after each row
 }
-
 ?>
 
