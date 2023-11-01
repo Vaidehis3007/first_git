eb6f1b9072750afaf6d299d7b4644b7dc0e2bf19
“Vaidehie” <sahuvaidehi30@gmail.com>
2023-11-01 20:47:20 +0530

    Add Song class

---
+++ practicals/Song.php
@@ -0,0 +1,67 @@
+<?php
+
+class Song {
+    // Properties
+    private $title;
+    private $artist;
+    private $genre;
+    private $tempo;
+
+    // Constructor
+    public function __construct($title, $artist, $genre, $tempo) {
+        $this->setTitle($title);
+        $this->setArtist($artist);
+        $this->setGenre($genre);
+        $this->setTempo($tempo);
+    }
+
+    // Getter for title
+    public function getTitle() {
+        return $this->title;
+    }
+
+    // Setter for title
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    // Getter for artist
+    public function getArtist() {
+        return $this->artist;
+    }
+
+    // Setter for artist
+    public function setArtist($artist) {
+        $this->artist = $artist;
+    }
+
+    // Getter for genre
+    public function getGenre() {
+        return $this->genre;
+    }
+
+    // Setter for genre
+    public function setGenre($genre) {
+        $this->genre = $genre;
+    }
+
+    // Getter for tempo
+    public function getTempo() {
+        return $this->tempo;
+    }
+
+    // Setter for tempo
+    public function setTempo($tempo) {
+        $this->tempo = $tempo;
+    }
+}
+
+// Example usage:
+$mySong = new Song("Ivy", "Frank Ocean", "Indie pop", 120);
+echo "Title: " . $mySong->getTitle() . "\n";
+echo "Artist: " . $mySong->getArtist() . "\n";
+echo "Genre: " . $mySong->getGenre() . "\n";
+echo "Tempo: " . $mySong->getTempo() . "\n";
+
+?>
+
