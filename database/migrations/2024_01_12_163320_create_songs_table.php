fd0be8488b4a38a0f9e075ae0131d4971a798bca
“Vaidehie” <sahuvaidehi30@gmail.com>
2024-01-12 22:23:01 +0530

    Create Song database model

--- database/migrations/2024_01_12_163320_create_songs_table.php
+++ database/migrations/2024_01_12_163320_create_songs_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('songs', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+	    $table->timestamps();
+	    $table->string('title');
+            $table->string('artist');
         });
     }
 
--- database/seeders/SongSeeder.php
+++ database/seeders/SongSeeder.php
@@ -4,14 +4,26 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Song;
 class SongSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+
+        // Seed a few songs
+        Song::create([
+            'artist' => 'BlankPink',
+            'title' => 'Ice Cream',
+        ]);
+
+        Song::create([
+            'artist' => 'Mxmtoon',
+            'title' => 'Monalisa',
+        ]);
+
     }
 }
