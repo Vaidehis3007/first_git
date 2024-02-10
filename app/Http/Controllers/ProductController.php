e15eeded3abc407208f4d29361e609aafc04cfca
“Vaidehie” <sahuvaidehi30@gmail.com>
2024-02-10 22:01:29 +0530

    ProductController was modified.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,6 +13,29 @@ class ProductController extends Controller
     {
         return view('products.create');
     }
+    public function edit(string $id)
+    {
+        $product = Product::findOrFail($id);
+        return view('products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $product = Product::findOrFail($id);
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric|min:0',
+        ]);
+        $product->update($validatedData);
+        return redirect()
+            ->route('products.index')
+            ->with('success', 'Product updated successfully.');
+    }
+
     public function store(Request $request)
     {
         $validatedData = $request->validate([
