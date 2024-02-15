bacaf276833c5d78819af2a0fd09ea5debbc0141
“Vaidehie” <sahuvaidehi30@gmail.com>
2024-02-15 23:25:12 +0530

    Add test cases for Practical and Song classes

---
+++ SongTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Practicals\Song;
+
+class SongsTest extends TestCase
+{
+    public function testSetTempo(): void
+    {
+        $song = new Song("Title", "Artist", "Genre", 120);
+        $song->setTempo(60);
+        $result = $song->getTempo();
+        $this->assertEquals(60, $result);
+    }
+    public function testSetTempoAcceptsIntegerString(): void
+    {
+        $song = new Song("Title", "Artist", "Genre", 120);
+        $song->setTempo("60");
+        $result = $song->getTempo();
+        $this->assertEquals(60, $result);
+    }
+    public function testSetTempoRejectsAlphabetString(): void
+    {
+        $song = new Song("Title", "Artist", "Genre", 120);
+        $this->expectException(\InvalidArgumentException::class);
+        $song->setTempo("60 BPM");
+    }
+    public function testSetTempoRejectsFloat(): void
+    {
+        $song = new Song("Title", "Artist", "Genre", 120);
+        $this->expectException(\InvalidArgumentException::class);
+        $song->setTempo(60.5);
+    }
+
+    public function testSongsOk(): void
+    {
+        $response = $this->get('/songs');
+        $response->assertStatus(200);
+    }
+    public function testSongs_staticOk(): void
+    {
+        $response = $this->get('/songs_static');
+    $response->assertStatus(200);
+    }
+    
+    
+}
